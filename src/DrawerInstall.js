bcb7f64c99082cac7179bbb93fb3c8467c27ebde
usamahbass <basalamahusamah7@gmail.com>
2022-06-27 00:42:22 +0700

    change: to css

--- src/DrawerInstall.js
+++ src/DrawerInstall.js
@@ -1,48 +1,6 @@
 import React from "react";
-import { makeStyles } from "@mui/styles";
 import { Drawer, Button, Typography, Box } from "@mui/material";
-
-const useDrawerInstallStyles = makeStyles(() => ({
-  customDrawerPaper: {
-    width: "100% !important",
-    height: "370px !important",
-    display: "block !important",
-    margin: "0 auto !important",
-    maxWidth: "480px !important",
-    backgroundColor: "#5DC3B2 !important",
-    boxShadow: "none !important",
-    border: "0 !important",
-    right: "12px !important",
-    overflow: "hidden !important",
-  },
-  customBtnInstall: {
-    width: "300px !important",
-    height: "50px !important",
-    backgroundColor: "white !important",
-    color: "#5DC3B2 !important",
-    fontWeight: "600 !important",
-    borderRadius: "20px !important",
-    marginTop: "1.5rem !important",
-    "&:hover": {
-      backgroundColor: "white !important",
-      opacity: "0.8 !important",
-    },
-  },
-  customBtnClose: {
-    width: "300px !important",
-    height: "50px !important",
-    backgroundColor: "#5DC3B2 !important",
-    color: "white !important",
-    border: "1px solid #fff !important",
-    fontWeight: "600 !important",
-    borderRadius: "20px !important",
-    marginTop: "1.5rem !important",
-    "&:hover": {
-      backgroundColor: "#5DC3B2 !important",
-      opacity: "0.8 !important",
-    },
-  },
-}));
+import "./drawer.css";
 
 const DrawerInstall = ({
   open,
@@ -52,12 +10,11 @@ const DrawerInstall = ({
   installTitle,
   closeTitle,
 }) => {
-  const classes = useDrawerInstallStyles();
   return (
     <Drawer
       anchor="bottom"
       open={open}
-      classes={{ paper: classes.customDrawerPaper }}
+      classes={{ paper: "custom-drawer-paper" }}
     >
       <Box
         gap="5"
@@ -72,7 +29,7 @@ const DrawerInstall = ({
           alignn="center"
           style={{
             color: "white",
-            fontSize: '1.15rem',
+            fontSize: "1.15rem",
             textAlign: "center",
             fontFamily: `'Poppins', sans-serif`,
           }}
@@ -80,10 +37,10 @@ const DrawerInstall = ({
           {description}
         </Typography>
 
-        <Button className={classes.customBtnInstall} onClick={handleInstall}>
+        <Button className="custom-btn-install" onClick={handleInstall}>
           {installTitle}
         </Button>
-        <Button className={classes.customBtnClose} onClick={onClose}>
+        <Button className="custom-btn-close" onClick={onClose}>
           {closeTitle}
         </Button>
       </Box>
